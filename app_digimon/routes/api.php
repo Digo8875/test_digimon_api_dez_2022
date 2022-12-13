a6f04c6b69a28f9ff732facc023a626100282af1
Rodrigo Cândido <rodrigo_piranguinho@hotmail.com>
2022-12-13 14:02:27 -0300

    refactor(routes): Adjust route names to API

--- app_digimon/routes/api.php
+++ app_digimon/routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/digimons', [App\Http\Controllers\DigimonApiController::class, 'getDigimons'])->name('get_digimons');
+Route::get('/digimons', [App\Http\Controllers\DigimonApiController::class, 'getDigimons'])->name('api_get_digimons');
 
-Route::get('/digimons/level/{level}', [App\Http\Controllers\DigimonApiController::class, 'getDigimonsByLevel'])->name('get_digimons_by_level');
+Route::get('/digimons/level/{level}', [App\Http\Controllers\DigimonApiController::class, 'getDigimonsByLevel'])->name('api_get_digimons_by_level');
 
-Route::get('/digimon/name/{name}', [App\Http\Controllers\DigimonApiController::class, 'getDigimonByName'])->name('get_digimon_by_name');
+Route::get('/digimon/name/{name}', [App\Http\Controllers\DigimonApiController::class, 'getDigimonByName'])->name('api_get_digimon_by_name');
